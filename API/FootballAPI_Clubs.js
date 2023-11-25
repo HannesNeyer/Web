f6e5a5fec86d6e919708aefee9e0c2ef97d11a1d
HannesNeyer <hannes.neyer@student.htldornbirn.at>
2023-11-25 14:24:15 +0100

    Fixed a problem with texts over each other

--- API/FootballAPI_Clubs.js
+++ API/FootballAPI_Clubs.js
@@ -7,57 +7,66 @@ async function getData() {
         }
         const data = await response.json();
         let html ="";
+        html += "<h1>Zahlen</h1>"
         data.slice(0, 4).forEach((element, index) => {
-            html += "<h1>Zahlen</h1>" + "<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
+            html +="<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
         });
         document.getElementById("output").innerHTML=html;
 
         html ="";
+        html += "<h1>B</h1>"
         data.slice(4, 7).forEach((element, index) => {
-            html += "<h1>B</h1>" + "<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
+            html += "<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
         });
         document.getElementById("output1").innerHTML=html;
 
         html ="";
+        html += "<h1>E</h1>"
         data.slice(7, 8).forEach((element, index) => {
-            html += "<h1>E</h1>" + "<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
+            html += "<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
         });
 
         document.getElementById("output2").innerHTML=html;
 
         html ="";
+        html += "<h1>F</h1>"
         data.slice(8, 10).forEach((element, index) => {
-            html += "<h1>F</h1>" + "<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
+            html += "<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
         });
         document.getElementById("output3").innerHTML=html;
 
         html ="";
+        html += "<h1>R</h1>" 
         data.slice(10, 11).forEach((element, index) => {
-            html += "<h1>R</h1>" + "<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
+            html += "<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
         });
         document.getElementById("output4").innerHTML=html;
 
         html ="";
+        html += "<h1>S</h1>"
         data.slice(11, 13).forEach((element, index) => {
-            html += "<h1>S</h1>" + "<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
+            html += "<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
         });
         document.getElementById("output5").innerHTML=html;
 
         html ="";
+        html += "<h1>T</h1>"
         data.slice(13, 14).forEach((element, index) => {
-            html += "<h1>T</h1>" + "<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
+            html += "<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
         });
         document.getElementById("output6").innerHTML=html;
 
         html ="";
+        html += "<h1>V</h1>"
         data.slice(14, 17).forEach((element, index) => {
-            html += "<h1>V</h1>" + "<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
+            html += "<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
         });
         document.getElementById("output7").innerHTML=html;
 
         html ="";
+        html += "<h1>W</h1>"
         data.slice(17, 18).forEach((element, index) => {
-            html += "<h1>W</h1>" + "<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
+            html += "<div class='content'>" + "<div class='icons'><img src=" + element.teamIconUrl + " width='80vw' height='80vw'></div>" +  "<div class ='teamName'>" + element.teamName + "</div>" + "</div>" 
         });
         document.getElementById("output8").innerHTML=html;
         // process your data further8
