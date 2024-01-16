4e98cb18000b5e660a51688a11e9b672ba3907d7
HannesNeyer <hannes.neyer@student.htldornbirn.at>
2024-01-16 09:12:02 +0100

    Video 3 and 4 as well as React Router installation

--- demo/src/components/PeopleContainer.js
+++ demo/src/components/PeopleContainer.js
@@ -4,18 +4,30 @@ import Card from './card'
 export default function PeopleContainer() {
 
   const [people, setPeople] = useState([]);
+  const [filteredPeople, setFilteredPeople] = useState([]);
+  const filterPeople = (filter) => {
+    let filtered = people.filter(person=> person.name.toLowerCase().includes(filter.toLowerCase()));
+    setFilteredPeople(filtered);
+  }
 
   useEffect(()=>{
     fetch("https://659fddef5023b02bfe8aa81a.mockapi.io/api/person/person").then((res) => res.json().then((data)=>{
       setPeople(data);
+      setFilteredPeople(data);
     })
     );
   },[])
 
   return (
-    <div>
-        <div className="grid grid-cols-4 lg:grid-cols-4 gap-4">
-          {people.map((person) => {
+    <div className='p-8'>
+      <div className='border mb-4 fixed bg-white w-full h-28'>
+        <h1>People</h1>
+        <input className='border p-5' type="text" placeholder='search' onChange={(element)=>{
+          filterPeople(element.target.value);
+        }}></input>
+      </div>
+        <div className="grid grid-cols-4 lg:grid-cols-4 gap-4 p-8 pt-32">
+          {filteredPeople.map((person) => {
               return <Card name={person.name} title={person.jobtitle} imageUrl={person.avatar}/>
           })}
         </div>
