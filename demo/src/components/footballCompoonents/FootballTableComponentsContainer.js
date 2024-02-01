3ee2f55aac833890efcd97531073148410ab6919
HannesNeyer <hannes.neyer@student.htldornbirn.at>
2024-02-01 19:07:42 +0100

    Changed Data margins, placement still missing

--- demo/src/components/footballCompoonents/FootballTableComponentsContainer.js
+++ demo/src/components/footballCompoonents/FootballTableComponentsContainer.js
@@ -3,6 +3,7 @@ import TableContent from './TableContent';
 
 export default function FootballTableComponentsContainer() {
     const [table, setTable] = useState([]);
+    const [place, setPlace] = useState([]);
 
     useEffect(() => {
       fetch("https://api.openligadb.de/getbltable/bl1/2023").then((res) => res.json().then((data) => {
@@ -12,9 +13,8 @@ export default function FootballTableComponentsContainer() {
     }, [])
 
     return (
-    <div className='relative w-[1012px] h-[500px] ml-[19.1vw] justify-center font-sans-serif 
+    <div className='relative w-[1012px] h-[500px] ml-[293px] justify-center font-sans-serif 
         text-[15px] text-white ml-[30px] overflow-y-scroll'>
-
         {table.map((match) => {
           return  <TableContent teamName={match.teamName} teamIconUrl={match.teamIconUrl} matches={match.matches} won={match.won} draw={match.draw} 
           lost={match.lost} goals={match.goals} goalDiff={match.goalDiff} points={match.points}/>
--- demo/src/components/footballCompoonents/TableContent.js
+++ demo/src/components/footballCompoonents/TableContent.js
@@ -4,14 +4,14 @@ export default function TableContent({teamName, teamIconUrl, matches, won, draw,
   return (
     <div className='flex w-full h-[40px] mt-[10px] rounded-[8px] bg-gradient-to-r from-[#F90000] to-[#BC0606] text-center items-center'>
       <img className='h-[2.6vw] w-[2.6vw]' src={teamIconUrl}/>
-      <div className='bg-black w-[200px] text-left'>{teamName}</div>
-      <div className=''>{matches}</div>
-      <div className=''>{won}</div>
-      <div className=''>{draw}</div>
-      <div className=''>{lost}</div>
-      <div className=''>{goals}</div>
-      <div className=''>{goalDiff}</div>
-      <div className=''>{points}</div>
+      <div className='w-[200px] text-left'>{teamName}</div>
+      <div className='ml-[20px]'>{matches}</div>
+      <div className='ml-[125px] w-[20px]'>{won}</div>
+      <div className='ml-[2px] w-[20px]'>{draw}</div>
+      <div className='ml-[2px] w-[20px]'>{lost}</div>
+      <div className='ml-[117px]'>{goals}</div>
+      <div className='ml-[115px] w-[20px]'>{goalDiff}</div>
+      <div className='ml-[125px]'>{points}</div>
     </div>
   )
 }
