e680b71d944287f0ae860d455888c6dd78108a90
HannesNeyer <hannes.neyer@student.htldornbirn.at>
2024-02-13 09:16:24 +0100

    Final verion of the Ract Football Table

--- demo/src/components/footballCompoonents/FootballTableComponentsContainer.js
+++ demo/src/components/footballCompoonents/FootballTableComponentsContainer.js
@@ -2,23 +2,22 @@ import React, { useEffect, useState } from 'react'
 import TableContent from './TableContent';
 
 export default function FootballTableComponentsContainer() {
-    const [table, setTable] = useState([]);
-    const [place, setPlace] = useState([]);
+  const [table, setTable] = useState([]);
 
-    useEffect(() => {
-      fetch("https://api.openligadb.de/getbltable/bl1/2023").then((res) => res.json().then((data) => {
-        setTable(data);
-      })
-      );
-    }, [])
+  useEffect(() => {
+    fetch("https://api.openligadb.de/getbltable/bl1/2023").then((res) => res.json().then((data) => {
+      setTable(data);
+    })
+    );
+  },[])
 
-    return (
-    <div className='relative w-[1012px] h-[500px] ml-[293px] justify-center font-sans-serif 
-        text-[15px] text-white ml-[30px] overflow-y-scroll'>
-        {table.map((match) => {
-          return  <TableContent teamName={match.teamName} teamIconUrl={match.teamIconUrl} matches={match.matches} won={match.won} draw={match.draw} 
+  return (
+      <div className='relative w-[1013px] h-[500px] ml-[292px] justify-center font-sans-serif 
+      text-[15px] text-white ml-[30px] overflow-y-scroll'>
+        {table.map((match, index=0) => {
+          return  <TableContent placement={index+1} teamName={match.teamName} teamIconUrl={match.teamIconUrl} matches={match.matches} won={match.won} draw={match.draw} 
           lost={match.lost} goals={match.goals} goalDiff={match.goalDiff} points={match.points}/>
-        })}
+      })}
     </div>
   )
 }
--- demo/src/components/footballCompoonents/TableContent.js
+++ demo/src/components/footballCompoonents/TableContent.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default function TableContent({teamName, teamIconUrl, matches, won, draw, lost, goals, goalDiff, points}) {
+export default function TableContent({placement, teamName, teamIconUrl, matches, won, draw, lost, goals, goalDiff, points}) {
   return (
     <div className='flex w-full h-[40px] mt-[10px] rounded-[8px] bg-gradient-to-r from-[#F90000] to-[#BC0606] text-center items-center'>
-      <img className='h-[2.6vw] w-[2.6vw]' src={teamIconUrl}/>
-      <div className='w-[200px] text-left'>{teamName}</div>
+      <div className='w-[30px]'>{placement}</div>
+      <img className='h-[2.6vw] w-[2.6vw]' src={teamIconUrl} alt="Image Loading"/>
+      <div className='w-[173px] text-left'>{teamName}</div>
       <div className='ml-[20px]'>{matches}</div>
       <div className='ml-[125px] w-[20px]'>{won}</div>
       <div className='ml-[2px] w-[20px]'>{draw}</div>
